d4e66b743529e5d6917096d8b7f6113a4ba0415a
Charan Kumar <charan.marasani007@gmail.com>
2021-11-11 11:21:21 +0530

    Removed changes that were added for overlay issue.

--- app/src/main/java/com/example/burstgallery/MainActivity.java
+++ app/src/main/java/com/example/burstgallery/MainActivity.java
@@ -3,10 +3,7 @@ package com.example.burstgallery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity {
     private static final String webViewUrl   = "https://www.burst.com/webapps/widgets/gallery/cards?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaWQiOiJZSEdIRjdKODczNEgzS1AwMzdHS19JbnRlcm5hbFB1YmxpYyIsImJpZCI6IjUxNDgxNDc3MzU3NDQ3MTAwMCIsImJ0cCI6IkEiLCJ0dHAiOiJHIiwiaXNzIjoiYnVyc3Qud2lkZ2V0IiwiaWF0IjoxNTc0NzY5NTg1LCJleHAiOjE2Mzc4NDE1ODV9.b7LODWnmxK-oJ1TzAa8_vGPpgGXKmuBzezk469ikWUg";
@@ -32,17 +29,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
         webView.onActivityResult(requestCode, resultCode, intent);
-
-        if (
-                Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE
-        ) {
-            if (!Settings.canDrawOverlays(this)) {
-                Intent overlayIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                        Uri.parse("package:" + getPackageName()));
-                startActivityForResult(overlayIntent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE);
-            }
-        }
     }
     /* end  */
 }
